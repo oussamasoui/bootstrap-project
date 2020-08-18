d4ae6aac1a87e05b0229cc536595dc9693f0be0e
oussamasoui <souioussama86@gmail.com>
2020-08-18 12:37:38 +0100

    img showed

---
+++ proj.js
@@ -0,0 +1,36 @@
+
+	
+	$("#img1").click(function(){
+	$(".cont").hide();
+	$("#img2").hide();
+	$("#img3").hide();
+	
+	var value = $('#cc').val();
+	var tempElement = $("<span></span>");
+	tempElement.text(value);
+	tempElement.css({"color":"DarkRed","font-family":"Butcherman", "font-size": "48px", "text-shadow": "4px 4px 4px #0000CD"});
+	$(".centered").append(tempElement)
+
+});
+	$("#img2").click(function(){
+	$(".cont").hide();
+	$("#img1").hide();
+	$("#img3").hide();	
+	var value = $('#cc').val();
+	var second = $("<span></span>");
+	second.text(value);
+	second.css({"color":"white","font-family":"Dancing", "font-size": "48px", "text-shadow": "4px 4px 4px #FFFAFA"});
+	$(".centered").append(second)
+
+});
+	$("#img3").click(function(){
+	$(".cont").hide();
+	$("#img1").hide();
+	$("#img2").hide();	
+	var value = $('#cc').val();
+	var therd = $("<span></span>");
+	therd.text(value);
+	therd.css({"color":"white","font-family":"Dancing", "font-size": "48px", "text-shadow": "4px 4px 4px #800000"});
+	$(".centered").append(therd)
+
+});
